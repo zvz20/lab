899c29f87aa14b14cb711f2078f18845ea37c45e
zvz20 <57296118+zvz20@users.noreply.github.com>
2021-12-08 14:16:14 +0200

    Add files via upload

---
+++ project/js/init.js
@@ -0,0 +1,7 @@
+(function($){
+  $(function(){
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.modal').modal();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
